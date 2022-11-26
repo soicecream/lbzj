a033f87cfbe71f250ed7a5a91a05f3eb8e1ce940
老实滴j <2644401754@qq.com>
2022-11-26 14:41:14 +0800

    用户

---
+++ web/src/views/oj/work/worknotice.vue
@@ -0,0 +1,32 @@
+<template>
+  <el-card class="box-card" style="width: 95%;margin: 0.5rem auto;">
+    <span>{{list.notice}}</span>
+  </el-card>
+</template>
+
+<script>
+import {getWork} from "@/api/work";
+
+export default {
+  name: "worknotice",
+  data(){
+    return{
+      list:null,
+    }
+  },
+  methods:{
+    getList(){
+      getWork(this.$route.params.id.toString()).then(response=>{
+        this.list = response.data
+      })
+    }
+  },
+  created() {
+    this.getList()
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
