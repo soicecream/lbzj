b44fb1b1fbb4896bd08f508b2e0e74eb7901bb62
soicecream <2657040829@qq.com>
2023-04-25 00:06:27 +0800

    permission.js

--- web/src/store/modules/permission.js
+++ web/src/store/modules/permission.js
@@ -1,88 +1,90 @@
-import { constantRoutes } from '@/router'
+import {constantRoutes} from '@/router'
 import {getRouters} from "@/api/user";
 import AdminIndex from '@/views/admin/index';
 
 const permission = {
 
-  state: {
-    routes: [],
-    addRoutes: [],
-    defaultRoutes: [],
-    topbarRouters: [],
-    sidebarRouters: []
-  },
-  mutations: {
-    SET_ROUTES: (state, routes) => {
-      state.addRoutes = routes
-      state.topbarRouters = routes
+    state: {
+        routes: [],
+        addRoutes: [],
+        defaultRoutes: [],
+        topbarRouters: [],
+        sidebarRouters: []
     },
-    SET_DEFAULT_ROUTES: (state, routes) => {
-      state.defaultRoutes = constantRoutes.concat(routes)
+    mutations: {
+        SET_ROUTES: (state, routes) => {
+            state.addRoutes = routes
+            state.topbarRouters = routes
+        },
+        SET_DEFAULT_ROUTES: (state, routes) => {
+            state.defaultRoutes = constantRoutes.concat(routes)
+        },
+        SET_TOPBAR_ROUTES: (state, routes) => {
+            // 顶部导航菜单默认添加统计报表栏指向首页
+            const index = [{
+                path: 'index',
+                meta: {title: '统计报表', icon: 'dashboard'}
+            }]
+            state.topbarRouters = routes.concat(index);
+        },
+        SET_SIDEBAR_ROUTERS: (state, routes) => {
+            state.sidebarRouters = routes
+        },
     },
-    SET_TOPBAR_ROUTES: (state, routes) => {
-      // 顶部导航菜单默认添加统计报表栏指向首页
-      const index = [{
-        path: 'index',
-        meta: { title: '统计报表', icon: 'dashboard'}
-      }]
-      state.topbarRouters = routes.concat(index);
-    },
-    SET_SIDEBAR_ROUTERS: (state, routes) => {
-      state.sidebarRouters = routes
-    },
-  },
-  actions: {
-    // 生成路由
-    GenerateRoutes({ commit }) {
-      return new Promise(resolve => {
-        // 向后端请求路由数据
-        getRouters().then(res => {
-          const sdata = JSON.parse(JSON.stringify(res.data))
-          const sidebarRoutes = filterAsyncRouter(sdata)
-          const s=filterAsyncRouter(JSON.parse(JSON.stringify(res.data)))
-          for(var i=0;i<3;i++){
-            for (var j=0;j<sidebarRoutes[i].children.length;j++){
-              if(sidebarRoutes[i].children[j].menuName==="修改问题") {
-                sidebarRoutes[i].children.splice(j,1);
-              }
-            }
-          }
-          console.log(s)
-          commit('SET_ROUTES', sidebarRoutes)
-          resolve(s)
-        })
-      })
+    actions: {
+        // 生成路由
+        GenerateRoutes({commit}) {
+            return new Promise(resolve => {
+                // 向后端请求路由数据
+                getRouters().then(res => {
+                    const sdata = JSON.parse(JSON.stringify(res.data))
+                    const sidebarRoutes = filterAsyncRouter(sdata)
+                    const s = filterAsyncRouter(JSON.parse(JSON.stringify(res.data)))
+                    for (var i = 0; i < 3; i++) {
+                        if (sidebarRoutes[i] && sidebarRoutes[i].children) {
+                            for (var j = 0; j < sidebarRoutes[i].children.length; j++) {
+                                if (sidebarRoutes[i].children[j].menuName === "修改问题") {
+                                    sidebarRoutes[i].children.splice(j, 1);
+                                }
+                            }
+                        }
+                    }
+                    console.log(s)
+                    commit('SET_ROUTES', sidebarRoutes)
+                    resolve(s)
+                })
+            })
+        }
     }
-  }
 }
 
 // 遍历后台传来的路由字符串，转换为组件对象
 function filterAsyncRouter(asyncRouterMap, lastRouter = false, type = false) {
-  return asyncRouterMap.filter(route => {
+    return asyncRouterMap.filter(route => {
 
-      // Layout ParentView 组件特殊处理
-      if (!route.component) {
-        route.component = AdminIndex
-      } else {
-        route.component = loadView(route.component)
-    }
-    if (route.children != null && route.children && route.children.length) {
-      route.children = filterAsyncRouter(route.children, route, type)
-    } else {
-      delete route['children']
-      delete route['redirect']
-    }
-    return true
-  })
+        // Layout ParentView 组件特殊处理
+        if (!route.component) {
+            route.component = AdminIndex
+        } else {
+            route.component = loadView(route.component)
+        }
+        if (route.children != null && route.children && route.children.length) {
+            route.children = filterAsyncRouter(route.children, route, type)
+        } else {
+            delete route['children']
+            delete route['redirect']
+        }
+        return true
+    })
 }
 
 export const loadView = (view) => {
-  if (process.env.NODE_ENV === 'development') {
-    return (resolve) => require([`@/views/${view}`], resolve)
-  } else {
-    // 使用 import 实现生产环境的路由懒加载
-    return () => import(`@/views/${view}`)
-  }
+    if (process.env.NODE_ENV === 'development') {
+        return (resolve) => require([`@/views/${view}`], resolve)
+    } else {
+        // 使用 import 实现生产环境的路由懒加载
+        return () => import(`@/views/${view}`)
+    }
 }
 
 export default permission
